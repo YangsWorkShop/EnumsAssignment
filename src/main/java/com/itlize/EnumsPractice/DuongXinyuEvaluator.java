7a01f9eb21b828272880d17aa5ca3a1ed544a132
Sang <sangnguyen5697@gmail.com>
2020-02-09 11:04:16 -0500

    passed all tests

---
+++ src/main/java/com/itlize/EnumsPractice/DuongXinyuEvaluator.java
@@ -0,0 +1,16 @@
+package com.itlize.EnumsPractice;
+
+public class DuongXinyuEvaluator {
+    private String input;
+    public DuongXinyuEvaluator(String input) {
+        this.input = input;
+    }
+
+    public boolean isDuong() {
+        return input.equalsIgnoreCase("Duong");
+    }
+
+    public boolean isXinyu() {
+        return input.equalsIgnoreCase("Xinyu");
+    }
+}
